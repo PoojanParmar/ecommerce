78830586fef838743326dd01e08fd50fc39bb332
RishiDhaduk <rishidhaduk25@gmail.com>
2025-01-24 03:39:47 -0500

    Add to Caet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Import Link from react-router-dom
 import CategoryList from './components/CategoryList';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
-import Cart from './components/Cart';
+import Cart from './components/Cart'; // Import the Cart component for separate route
 import Checkout from './components/Checkout';
 import AdminLogin from './components/AdminLogin';
 import AdminDashboard from './components/AdminDashboard';
@@ -11,7 +11,6 @@ import OrderConfirmation from './components/OrderConfirmation';
 import { CartProvider } from './CartContext';
 import './App.css';
 import AccountPage from './components/AccountPage';
-
 
 function App() {
     const [selectedCategory, setSelectedCategory] = useState(null);
@@ -23,29 +22,30 @@ function App() {
                     <header className="header">
                         <div className="header-logo">CameraWaleBhaiya</div>
                         <input type="text" className="header-search" placeholder="Search products..." />
+                        
+                        {/* Add a link in the header to navigate to Cart page */}
                         <div className="header-cart">
-                            <Cart />
+                            <Link to="/cart">Go to Cart</Link>
                         </div>
                     </header>
+                    
                     <main>
                         <aside className="sidebar">
                             <CategoryList onSelectCategory={setSelectedCategory} />
-                            
                         </aside>
+                        
                         <section className="content">
                             <Routes>
-                                <Route 
-                                    path="/" 
-                                    element={<ProductList selectedCategory={selectedCategory} />} 
-                                    
-                                />
-                                 <Route path="/account" element={<AccountPage />} />
+                                <Route path="/" element={<ProductList selectedCategory={selectedCategory} />} />
+                                <Route path="/account" element={<AccountPage />} />
                                 <Route path="/product/:id" element={<ProductDetails />} />
                                 <Route path="/checkout" element={<Checkout />} />
                                 <Route path="/admin/login" element={<AdminLogin />} />
                                 <Route path="/admin/dashboard" element={<AdminDashboard />} />
-                                <Route path="/account" element={<AccountPage />} />
-                                <Route path="/order-confirmation/:id" element={<OrderConfirmation />} /> {/* Add Order Confirmation Route */}
+                                <Route path="/order-confirmation/:id" element={<OrderConfirmation />} />
+                                
+                                {/* Add route for Cart page */}
+                                <Route path="/cart" element={<Cart />} />
                             </Routes>
                         </section>
                     </main>
